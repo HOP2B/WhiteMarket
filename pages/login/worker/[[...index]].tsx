7238d8381ceef347888ab50ab716bd9eced70607
Khangai Jagaa <216835222+khangai1221@users.noreply.github.com>
2026-01-19 18:03:05 +0800

    first commit

---
+++ pages/login/worker/[[...index]].tsx
@@ -0,0 +1,23 @@
+import { SignIn } from "@clerk/nextjs";
+
+const WorkerLogin = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Sign in as Worker
+        </h2>
+      </div>
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <SignIn
+          path="/login/worker"
+          routing="path"
+          signUpUrl="/register"
+          redirectUrl="/"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default WorkerLogin;
